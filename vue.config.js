9cc1855afb6e4628cb1b8f2de97b50b9f964ce81
EgorZadorin <egozador@yandex.ru>
2023-03-18 14:48:20 +0100

    Conflict resolved

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Egor Zadorin\'s Website',
-      }),
-    ],
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Egor Zadorin\'s Website';
+      return args;
+    });
   },
 });
