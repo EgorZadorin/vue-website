0ae084e77100ddeb297768c0f104db8909c0b0dc
EgorZadorin <egozador@yandex.ru>
2023-03-18 17:23:58 +0100

    Checking CI/CD works as intended 3

--- src/components/WebsiteContent.vue
+++ src/components/WebsiteContent.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <div class="content-inner">
       <h2>About Me</h2>
-      <p>Hi, I'm Egor Zadorin, an aspiring software engineer with a strong background in Java/Spring Boot and AWS Web Services. As an M.Sc. student in Computer Science, I'm continually honing my skills and expertise to deliver innovative solutions in the rapidly evolving world of technology. Welcome to my personal website, where you can learn more about my experience and passion for creating impactful software</p>
+      <p>Hi, I'm Egor Zadorin, an aspiring software engineer with a strong background in Java/Spring Boot and AWS Web Services. As an M.Sc. student in Computer Science, I'm continually honing my skills and expertise to deliver innovative solutions in the rapidly evolving world of technology. Welcome to my personal website, where you can learn more about my experience and passion for creating impactful software.</p>
     </div>
   </div>
 </template>
