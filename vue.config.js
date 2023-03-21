df2423000c83f0b241312f6706c596ddc2a6059b
EgorZadorin <egozador@yandex.ru>
2023-03-21 21:55:38 +0100

    Favicon and Description

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'Egor Zadorin\'s Website';
+      args[0].title = 'Egor Zadorin';
       return args;
     });
   },
