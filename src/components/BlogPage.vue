35d21cfa8a9d0b74721a29664eb5892a839664f7
EgorZadorin <egozador@yandex.ru>
2023-03-22 12:38:56 +0100

    Updated .gitignore paths

--- src/components/BlogPage.vue
+++ src/components/BlogPage.vue
@@ -2,17 +2,51 @@
   <div class="content">
     <h2>Blog</h2>
     <div class="content-inner">
-      <!-- Add your blog layout and individual blog post components here -->
+      <BlogPostPreview v-for="post in posts" :key="post.id" :post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import BlogPostPreview from "@/components/BlogPostPreview.vue";
+
 export default {
   name: "BlogPage",
+  components: {
+    BlogPostPreview,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "Blog Post 1",
+          description: "This is the description of the first blog post.",
+          slug: "blog-post-1",
+        },
+        {
+          id: 2,
+          title: "Blog Post 2",
+          description: "This is the description of the second blog post.",
+          slug: "blog-post-2",
+        },
+        {
+          id: 3,
+          title: "Blog Post 3",
+          description: "This is the description of the third blog post.",
+          slug: "blog-post-3",
+        },
+        {
+          id: 4,
+          title: "Blog Post 4",
+          description: "This is the description of the fourth blog post.",
+          slug: "blog-post-4",
+        },
+      ],
+    };
+  },
 };
 </script>
-
 <style scoped>
 
 
---
+++ src/components/BlogPostPreview.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="blog-post-preview">
+    <a :href="'https://blog.egorzadorin.com/' + post.slug" target="_blank">
+      <h3>{{ post.title }}</h3>
+    </a>
+    <p>{{ post.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogPostPreview",
+  props: {
+    post: Object,
+  },
+};
+</script>
+
+<style scoped>
+.blog-post-preview {
+  margin-bottom: 20px;
+}
+
+.blog-post-preview h3 {
+  margin: 0;
+  color: #007bff;
+  text-decoration: none;
+}
+
+.blog-post-preview a {
+  text-decoration: none;
+}
+
+.blog-post-preview a:hover h3 {
+  text-decoration: underline;
+}
+
+.blog-post-preview p {
+  margin-top: 5px;
+}
+</style>
