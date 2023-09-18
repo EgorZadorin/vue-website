af260bde413cc3d9c792c551b59fe5ea5ce59888
EgorZadorin <egorzadorin04@gmail.com>
2023-09-18 21:56:23 +0200

    Change address + loading info

--- blog/src/components/Blog.vue
+++ blog/src/components/Blog.vue
@@ -8,7 +8,9 @@
             <input type="text" id="email" v-model="email" />
             <button @click="addEmail">Add Email</button>
         </div>
-        <div>
+      <div v-if="isLoading">Loading...</div>
+      <div v-if="error">{{ error }}</div>
+      <div>
             <h2>All Emails</h2>
             <button @click="fetchEmails">Fetch Emails</button>
             <ul>
@@ -25,40 +27,50 @@
 import axios from "axios";
 
 export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: "Blog",
-    data() {
-        return {
-            email: "",
-            emails: [],
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Blog",
+  data() {
+    return {
+      email: "",
+      emails: [],
+      isLoading: false, // to control the spinner
+      error: null, // to display error messages
+    };
+  },
+  methods: {
+    async addEmail() {
+      this.isLoading = true;
+      this.error = null;
+      try {
+        const emailData = {
+          id: 1, // Shouldn't this be dynamically generated?
+          email: this.email,
         };
+        await axios.post("https://6gest3ir9l.execute-api.eu-central-1.amazonaws.com/subscribers", emailData);
+        this.email = "";
+        await this.fetchEmails();  // Auto-fetch emails
+      } catch (error) {
+        this.error = "Failed to add email."; // Setting an error message
+      } finally {
+        this.isLoading = false;
+      }
     },
-    methods: {
-        async addEmail() {
-            try {
-                const emailData = {
-                    id: 1,
-                    email: this.email,
-                };
-                await axios.post("https://mrbgdx287a.execute-api.eu-central-1.amazonaws.com/subscribers", emailData);
-                this.email = "";
-            } catch (error) {
-                console.error(error);
-            }
-        },
-        async fetchEmails() {
-            try {
-                const response = await axios.get("https://mrbgdx287a.execute-api.eu-central-1.amazonaws.com/subscribers");
-                this.emails = response.data;
-            } catch (error) {
-                console.error(error);
-            }
-        },
+    async fetchEmails() {
+      this.isLoading = true;
+      this.error = null;
+      try {
+        const response = await axios.get("https://6gest3ir9l.execute-api.eu-central-1.amazonaws.com/subscribers");
+        this.emails = response.data;
+      } catch (error) {
+        this.error = "Failed to fetch emails."; // Setting an error message
+      } finally {
+        this.isLoading = false;
+      }
     },
+  },
 };
 </script>
 
-
 <style scoped>
-/* Add any styles for the Blog component here */
+/* Styles */
 </style>
